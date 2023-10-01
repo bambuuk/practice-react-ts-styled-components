31d917ac947c2307bbbdd2f79760dd2e9b1b99bc
Ivan <vanyok.korobka@ukr.net>
2023-10-02 00:21:15 +0300

    feat/upd way to image, add info for deploying

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import SpacerHeader from "./components/SpacerHeader";
 import { CardContainer, CardLeftSide, CardRightSide } from "./components/CardContainer";
 import Container from "./components/Container";
 import Button from "./components/Button";
+import parfume from './assets/chanel_perfume.png';
 
 function App() {
     return (
@@ -9,7 +10,7 @@ function App() {
             <CardContainer>
                 <>
                     <CardLeftSide>
-                        <img src="/assets/chanel_perfume.png" alt="plants and perfume" />
+                        <img src={parfume} alt="plants and perfume" />
                     </CardLeftSide>
                     <CardRightSide>
                         <>
